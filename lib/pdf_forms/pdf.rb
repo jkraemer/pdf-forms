7924f30ce811eaf39dccdae4ba5c0a43b8d76e84
Dominic Sisneros <dsisnero@gmail.com>
2012-03-22 14:46:23 -0600

    On windows, files with spaces need a double quote to make sure it is one argument

--- lib/pdf_forms/pdf.rb
+++ lib/pdf_forms/pdf.rb
@@ -1,20 +1,20 @@
 module PdfForms
   class Pdf
     attr_reader :path
-    
+
     def initialize(path, pdftk)
       @path = path
       @pdftk = pdftk
     end
-    
+
     def fields
       @fields ||= read_fields
     end
-    
+
     protected
-    
+
     def read_fields
-      field_output = @pdftk.call_pdftk "'#{path}'", 'dump_data_fields'
+      field_output = @pdftk.call_pdftk %Q("#{path}"), 'dump_data_fields'
       @fields = field_output.split(/^---\n/).map do |field_text|
         if field_text =~ /^FieldName: (\w+)$/
           $1
