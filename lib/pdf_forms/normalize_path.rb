0d0bee4d8199a2c6e2cfb60c8d77ef625a77c372
Jens Kraemer <jk@jkraemer.net>
2016-01-29 03:58:13 +0100

    Let Kernel::Pathname() handle the input to normalize_path.
    - refs #38

--- lib/pdf_forms/normalize_path.rb
+++ lib/pdf_forms/normalize_path.rb
@@ -5,8 +5,7 @@ module PdfForms
   module NormalizePath
 
     def normalize_path(path)
-      path = path.to_path if path.respond_to? :to_path
-      path.to_str
+      Pathname(path).to_path
     end
 
   end
