0d2243087cf0dd264ea2d332af14242dac6ea116
Jens Kraemer <jk@jkraemer.net>
2017-07-06 10:18:54 +0800

    fix for jruby - unpack result appears to still carry the utf-16 encoding

--- lib/pdf_forms/fdf_hex.rb
+++ lib/pdf_forms/fdf_hex.rb
@@ -26,7 +26,14 @@ module PdfForms
     def encode_value_as_hex(value)
       value = value.to_s
       utf_16 = value.encode('UTF-16BE', :invalid => :replace, :undef => :replace)
-      '<FEFF' + utf_16.unpack('H*').first.upcase + '>'
+      hex = utf_16.unpack('H*').first
+      hex.force_encoding 'ASCII-8BIT' # jruby
+      '<FEFF' + hex.upcase + '>'
+    end
+
+    # Fdf implementation encodes to ISO-8859-15 which we do not want here.
+    def encode_data(fdf)
+      fdf
     end
   end
 end
